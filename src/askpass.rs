5ea8c0e2bcf11719306ee3fd1a81f8dfdd1a31c4
Tony Finch <dot@dotat.at>
2021-10-29 20:10:28 +0100

    notes and todos

--- src/askpass.rs
+++ src/askpass.rs
@@ -3,6 +3,13 @@ use rpassword::*;
 
 pub type AskPass = Box<dyn FnMut() -> Result<String>>;
 
+// when there isn't a terminal, we might try to open /dev/tty (or
+// something like that on ununixy systems) before checking $DISPLAY
+// and running ssh-askpass ... the rpassword crate is a bit awkward
+// because its behaviour is rather different from ssh-add.
+//
+// another issue to think about is the typo retry loop
+
 pub fn for_file(key_file: &str) -> AskPass {
     let prompt = format!("\rEnter passphrase for {}: ", key_file);
     Box::new(move || {
